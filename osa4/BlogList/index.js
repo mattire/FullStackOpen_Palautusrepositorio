9816f502aaaab7609797ab122d24b64e71e9098a
mattire <alr2d4@hotmail.com>
2026-02-01 19:36:26 +0200

    4.2 work in progress

--- osa4/BlogList/index.js
+++ osa4/BlogList/index.js
@@ -1,48 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-//const blogsRouter = require('./controllers/blogs')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-//const mongoUrl = 'mongodb://localhost/bloglist'
-const mongoUrl = process.env.MONGODB_URI
-
-console.log('connecting to', mongoUrl)  
-
-mongoose.connect(mongoUrl, { family: 4, timeoutMS: 5000 }).then(() => {
-  console.log('connected to MongoDB')
-}).catch((error) => {
-  console.log('error connecting to MongoDB:', error.message)
-})
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const port = process.env.PORT || 3003
-//const PORT = 3003
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
