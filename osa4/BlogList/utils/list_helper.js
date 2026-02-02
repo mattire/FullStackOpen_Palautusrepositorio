efca99cf72aa1c7fa9a4b55cd5d7dab599d0bee4
mattire <alr2d4@hotmail.com>
2026-02-02 19:43:58 +0200

    4.3-4.5

---
+++ osa4/BlogList/utils/list_helper.js
@@ -0,0 +1,30 @@
+const dummy = (blogs) => {
+  // ...
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  let favorite = blogs[0]
+
+  for (const blog of blogs) {
+    if (blog.likes > favorite.likes) {
+      favorite = blog
+    }
+  }
+
+  return favorite
+}
+
+module.exports = {
+  dummy, 
+  totalLikes,
+  favoriteBlog,
+}
