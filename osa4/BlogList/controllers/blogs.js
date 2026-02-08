4b063564bf6245f56497505312f1c9dbd3a0a9fe
mattire <alr2d4@hotmail.com>
2026-02-08 11:03:29 +0200

    login, token auth, adding blogs with token

--- osa4/BlogList/controllers/blogs.js
+++ osa4/BlogList/controllers/blogs.js
@@ -1,12 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const Log = require('../utils/logger')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
 
 blogsRouter.get('/', async (request, response) => {
   //Blog.find({}).then((blogs) => {
   //  response.json(blogs)
   //})
-  const results = await Blog.find({})
+  const results = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.status(200).json(results)
 })
 
@@ -19,9 +22,28 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body;
   
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  //const user = await User.findById(body.userId)
+  
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
   if(!Object.hasOwn(body, 'url')){
     response.status(400).json({ error: 'content missing' })
   } 
@@ -31,14 +53,27 @@ blogsRouter.post('/', async (request, response, next) => {
   } 
   else 
   {
-    const blog = new Blog(request.body)
-    // console.log("**************");
-    // console.log(blog);
+    //const blog = new Blog(request.body)
+    const blog = new Blog( { 
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      //userId: user._id
+      user: user._id
+    })
+    
+    console.log("**************");
+    console.log(blog);
+    console.log(user._id);
+    console.log(user.id);
     if(!Object.hasOwn(blog, 'likes'))
     {
       blog.likes = 0
     }
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
 
     //blog.save().then((result) => 
