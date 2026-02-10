a9c63d9ce72851705d4911ffc7501d54717ab216
mattire <alr2d4@hotmail.com>
2026-02-10 21:52:40 +0200

    5.3. creating blogs frontend

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -10,7 +10,14 @@ const App = () => {
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
 
-  const loggedIn = user != null;
+  const [newBlog, setNewBlog] = useState({ title: '', author:'', url:''}) 
+  // const [title, setTitle] = useState('') 
+  // const [author, setAuthor] = useState('') 
+  // const [url, setUrl] = useState('') 
+
+  const lsUser = window.localStorage.getItem('user');
+  const loggedIn = lsUser != null;
+  const userOjb = JSON.parse(lsUser)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -18,19 +25,43 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (e) => {
-    console.log(e);
+  const handleNewBlog = async (e) => {
+    e.preventDefault()
+    console.log(newBlog.title, newBlog.author, newBlog.url);   
+    console.log(userOjb.token);
+    console.log(userOjb);
+    //const r = await blogService.postNewBlog(title, author, url, userOjb.id, userOjb.token)
+    const r = await blogService.postNewBlogObj(newBlog, userOjb.id, userOjb.token)
+    if(r.request.status==201){
+      console.log(r.data);
+      setNewBlog({ title: '', author:'', url:''})
+      // setTitle('')
+      // setUrl('')
+      // setAuthor('')
+      var newBlogs = blogs.concat(r.data)
+      setBlogs(newBlogs)
+    }
+  }
+
+  const handleLogin = async (e) => {
     e.preventDefault()
     try {
-      const user = loginService.login({ username, password })
-      setUser(user)
+      const user = await loginService.login({ username, password })
+      var jsonUser = JSON.stringify(user)
+      window.localStorage.setItem('user', jsonUser)
       setUsername('')
       setPassword('')
+      setUser(jsonUser)
     } catch (error) {
       console.log(error);
     }
-
   }
+
+  const handleLogout = async(e) => {
+    window.localStorage.removeItem('user', null);
+    setUser(null)
+  }
+
 
   if(!loggedIn){
     return (
@@ -66,6 +97,44 @@ const App = () => {
     return (
       <div>
         <h2>blogs</h2>
+        <div>
+          {userOjb.name} logged in<button onClick={()=> handleLogout()}>logout</button>
+        </div><br/>
+        <h2>create new</h2>
+        <form onSubmit={handleNewBlog}>
+
+        <label>
+          title:
+          <input
+            type="text"
+            value={newBlog.title}
+            onChange={({ target }) => setNewBlog({...newBlog, title: target.value})}
+            />
+            {/* onChange={({ target }) => setTitle(target.value)} */}
+        </label><br/>
+        <label>
+          author:
+          <input
+            type="text"
+            value={newBlog.author}
+            onChange={({ target }) => setNewBlog({...newBlog, author: target.value})}
+            />
+            {/* onChange={({ target }) => setAuthor(target.value)} */}
+        </label><br/>
+        <label>
+          url:
+          <input
+            type="text"
+            value={newBlog.url}
+            onChange={({ target }) => setNewBlog({...newBlog, url: target.value})}
+            />
+            {/* onChange={({ target }) => setUrl(target.value)} */}
+        </label><br/>
+        <button type="submit">create</button>
+        <br/><br/>
+
+        </form>
+
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { use } from 'react'
 const baseUrl = '/api/blogs'
 
 const getAll = () => {
@@ -6,4 +7,33 @@ const getAll = () => {
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const postNewBlogObj = async (newBlog, userid, token) => {
+  return await postNewBlog(newBlog.title, newBlog.author, newBlog.url, userid, token);   
+}
+
+const postNewBlog = async (title, author, url, userid, token) => {
+  try {    
+    const newBlog = {
+      'title' : title,
+      'author' : author,
+      'url' : url,
+      'likes' : 0,
+      'userId' : userid,
+    }
+    console.log(newBlog);
+    
+    const resp = await axios.post(baseUrl, newBlog, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    })
+    return resp
+  } catch (error) {
+    console.log(error);
+    return error
+  }
+
+}
+
+export default { getAll, postNewBlog, postNewBlogObj }
