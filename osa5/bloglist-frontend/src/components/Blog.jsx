d4a8bfb57e483f0f85d78649d055c80e1407ea05
mattire <alr2d4@hotmail.com>
2026-02-11 23:10:54 +0200

    5.7, view blogs work in prog

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  
+  const [viewing, SetViewState] = useState(false)
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const ViewBlog = (e) => {
+    SetViewState(!viewing)
+  }
+  if(viewing){
+    return (
+      <div style={blogStyle}>
+        {blog.title} <br/>
+        {blog.author} <br/>
+        {blog.url} <br/>
+        {blog.likes} <br/>
+        <button onClick={ViewBlog}>view</button>
+      </div>    
+    )
+  }
+  else {
+    return (
+    <div>
+      {blog.title} {blog.author} <button onClick={ViewBlog}>view</button>
+    </div>  
+    )
+  }
+}
+
 
 export default Blog
