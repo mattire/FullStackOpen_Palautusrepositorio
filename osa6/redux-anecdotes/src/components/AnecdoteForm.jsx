2d608818b4faa1064c2f5959edc3b612d5a7adba
mattire <alr2d4@hotmail.com>
2026-02-25 12:16:51 +0200

    cleanup

--- osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux' 
 import { createAnecdote } from '../reducers/anecdoteReducer' 
 import { notifChange } from '../reducers/notificationReducer'
-//import {showAct, hideAct} from '../reducers/notificationReducer'
 
 
 export const AnecdoteForm = () => {
@@ -15,9 +14,7 @@ export const AnecdoteForm = () => {
     dispatch(createAnecdote(content))
 
     const notifMsg = `You created anecdote ${content}`
-    //dispatch(notifChange(showAct(notifMsg)))
     dispatch(notifChange(notifMsg))
-    //setTimeout(function() { dispatch(notifChange(hideAct()))}, 5000)
     setTimeout(function() { dispatch(notifChange(''))}, 5000)
   };
 
